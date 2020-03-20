5e3bd77d71ca04605c9b0b941d5f9da9fd3204af
Justin Kolodny <jbk2042@g.rit.edu>
2020-03-20 02:44:21 -0400

    Wrote the essential functionality of the program, comments next.

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ import sys
 def main():
     word_one = sys.argv[1]
     word_two = sys.argv[2]
-    print(word_one, word_two)
+    alphabet_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                      "t", "u", "v", "w", "x", "y", "z"]
+    # print(word_one, word_two)
+    for character in range(0, len(word_one)):
+        which_letter = ord(word_one[character]) - 97
+        if alphabet_array[which_letter] == word_two[character]:
+            continue
+        else:
+            if alphabet_array[which_letter] != chr(which_letter+97):
+                print("false")
+                sys.exit(0)
+            else:
+                alphabet_array[which_letter] = word_two[character]
+    print("true")
 
 
 if __name__ == "__main__":
